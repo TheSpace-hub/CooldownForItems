a434d6f9ad8d036dabb2ea91a844e3ba3d5544f9
TheSpace-hub <78466469+TheSpace-hub@users.noreply.github.com>
2025-05-27 19:08:56 +0300

    Initial commit

---
+++ src/main/java/diachkov/sergey/cooldownForItems/CooldownForItems.java
@@ -0,0 +1,17 @@
+package diachkov.sergey.cooldownForItems;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CooldownForItems extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
