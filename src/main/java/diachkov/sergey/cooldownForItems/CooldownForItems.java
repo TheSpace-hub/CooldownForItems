58b1e595bb80e784a3250449ab181dadf554383f
TheSpace-hub <78466469+TheSpace-hub@users.noreply.github.com>
2025-05-27 19:21:07 +0300

    Список с предметами их задержка

--- src/main/java/diachkov/sergey/cooldownForItems/CooldownForItems.java
+++ src/main/java/diachkov/sergey/cooldownForItems/CooldownForItems.java
@@ -1,17 +1,23 @@
 package diachkov.sergey.cooldownForItems;
 
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CooldownForItems extends JavaPlugin {
 
-    @Override
-    public void onEnable() {
-        // Plugin startup logic
+    private void printItemsWithCooldown() {
+        getLogger().info("Список вещей с ограничениями:");
+        ConfigurationSection items = getConfig().getConfigurationSection("items");
+        assert items != null;
 
+        for (String item : items.getKeys(false)) {
+            getLogger().info("  " + item + ": " + items.getInt(item));
+        }
     }
 
     @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    public void onEnable() {
+        saveDefaultConfig();
+        printItemsWithCooldown();
     }
 }
