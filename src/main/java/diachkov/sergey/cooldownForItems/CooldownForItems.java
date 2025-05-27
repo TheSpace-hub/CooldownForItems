4b5d908e79babbed89d970d752de3c4caa0f3951
TheSpace-hub <78466469+TheSpace-hub@users.noreply.github.com>
2025-05-27 22:59:40 +0300

    Отсчёт задержки предметов

--- src/main/java/diachkov/sergey/cooldownForItems/CooldownForItems.java
+++ src/main/java/diachkov/sergey/cooldownForItems/CooldownForItems.java
@@ -1,10 +1,21 @@
 package diachkov.sergey.cooldownForItems;
 
 import diachkov.sergey.cooldownForItems.listeners.MainHandler;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class CooldownForItems extends JavaPlugin {
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+
+public final class CooldownForItems extends JavaPlugin implements Runnable {
+
+    private static Map<Player, Map<Material, Integer>> cooldowns = new HashMap<>();
 
     private void printItemsWithCooldown() {
         getLogger().info("Список вещей с ограничениями:");
@@ -16,11 +27,70 @@ public final class CooldownForItems extends JavaPlugin {
         }
     }
 
+    public static void addPlayerToCooldownsList(Player player) {
+        cooldowns.put(player, new HashMap<>());
+    }
+
+    public static void removePlayerFromCooldownList(Player player) {
+        cooldowns.remove(player);
+    }
+
+    public static void addCooldown(Plugin plugin, Player player, Material material) {
+        if (checkCooldown(plugin, player, material))
+            return;
+
+        String minecraftId = material.getKey().asString();
+        if (!plugin.getConfig().getConfigurationSection("items").isSet(minecraftId))
+            return;
+
+        int cooldown = plugin.getConfig().getConfigurationSection("items").getInt(minecraftId);
+
+        cooldowns.get(player).put(material, cooldown);
+    }
+
+    public static boolean checkCooldown(Plugin plugin, Player player, Material material) {
+        String minecraftId = material.getKey().asString();
+        if (!plugin.getConfig().getConfigurationSection("items").isSet(minecraftId))
+            return false;
+
+        return cooldowns.get(player).containsKey(material);
+    }
+
+    public static void everyTick(Plugin plugin) {
+        plugin.getLogger().info("Задержки:");
+        for (Player player : cooldowns.keySet()) {
+            Map<Material, Integer> playerCooldowns = cooldowns.get(player);
+            if (playerCooldowns == null) continue;
+
+            Iterator<Map.Entry<Material, Integer>> iterator = playerCooldowns.entrySet().iterator();
+            while (iterator.hasNext()) {
+                Map.Entry<Material, Integer> entry = iterator.next();
+                Material material = entry.getKey();
+                int time = entry.getValue();
+
+                plugin.getLogger().info("  " + material.name() + ": " + time);
+
+                if (time <= 0) {
+                    iterator.remove();
+                } else {
+                    entry.setValue(time - 1);
+                }
+            }
+        }
+    }
+
     @Override
     public void onEnable() {
         saveDefaultConfig();
         printItemsWithCooldown();
 
         getServer().getPluginManager().registerEvents(new MainHandler(this), this);
+
+        Bukkit.getScheduler().runTaskTimer(this, this, 0L, 1L);
+    }
+
+    @Override
+    public void run() {
+        everyTick(this);
     }
 }
--- src/main/java/diachkov/sergey/cooldownForItems/listeners/MainHandler.java
+++ src/main/java/diachkov/sergey/cooldownForItems/listeners/MainHandler.java
@@ -1,9 +1,13 @@
 package diachkov.sergey.cooldownForItems.listeners;
 
+import diachkov.sergey.cooldownForItems.CooldownForItems;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.Plugin;
+
 
 public class MainHandler implements Listener {
 
@@ -14,8 +18,22 @@ public class MainHandler implements Listener {
     }
 
     @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        CooldownForItems.addPlayerToCooldownsList(event.getPlayer());
+    }
+
+    @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
-        plugin.getLogger().info("Событие!");
+        Player player = event.getPlayer();
+        if (event.getItem() == null)
+            return;
+
+        String itemId = event.getItem().getType().getKey().asString();
+        if (!plugin.getConfig().getConfigurationSection("items").isSet(itemId))
+            return;
+
+        CooldownForItems.addCooldown(plugin, player, event.getItem().getType());
+
     }
 
 }
